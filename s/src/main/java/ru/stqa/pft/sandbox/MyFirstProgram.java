45fa8f1e925c7552336771221c2524068c00998d
Bakhtiyarov, Timur <timur.bakhtiyarov@x5.ru>
2020-01-16 18:03:20 +0300

    first commit

---
+++ s/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.sandbox;
+
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        hello("world");
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+        Rectangle r = new Rectangle(4,5);
+        System.out.println("Площадь прямоугольника " + r.a + " на " + r.b + " = " + r.area());
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+
+}

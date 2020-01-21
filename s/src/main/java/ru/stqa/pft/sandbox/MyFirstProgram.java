0129591587395add390bb8a46a748a028f3199b7
Bakhtiyarov, Timur <timur.bakhtiyarov@x5.ru>
2020-01-21 17:26:41 +0300

    1 hw

--- s/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ s/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,18 +1,19 @@
 package ru.stqa.pft.sandbox;
 
+import static ru.stqa.pft.sandbox.Point.distance;
+
 public class MyFirstProgram {
     public static void main(String[] args) {
-        hello("world");
+
         Square s = new Square(5);
         System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-        Rectangle r = new Rectangle(4,5);
+        Rectangle r = new Rectangle(4, 5);
         System.out.println("Площадь прямоугольника " + r.a + " на " + r.b + " = " + r.area());
-    }
 
-    public static void hello(String somebody) {
-        System.out.println("Hello, " + somebody + "!");
+        Point p1 = new Point(2, 4);
+        Point p2 = new Point(-1, 10);
+        System.out.println("Расстояние между точками р1 (" + p1.x + "; " + p1.y + ") и p2 (" + p2.x + "; " + p2.y + ") = " + distance(p1, p2));
     }
-
 
 }
